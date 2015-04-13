b034090dc8b9bac4fcdf1befd4da8129e3430725
Fajri Adha <fajrilef@yahoo.com>
2015-04-13 21:33:12 +0700

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,21 @@
+package animal;
+
+/**
+ *
+ * @author fajri
+ */
+public class demo {
+    public static void main(String[] args){
+        cat kucing = new cat();
+        fish ikan = new fish();
+        spider laba = new spider();
+        
+        kucing.eat();
+        kucing.walk();
+        
+        ikan.play();
+        ikan.walk();
+        
+        laba.eat();
+    }
+}
